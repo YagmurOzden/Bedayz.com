086bf941959f51a11e6d903f2b24d38ddc7bd78c
yagmurozden <yagmurrrozdennn@gmail.com>
2020-06-11 06:11:44 +0300

    entity framework added

---
+++ Controllers/TodoItemController.cs
@@ -0,0 +1,90 @@
+﻿using bedayzAPI.Entities;
+using bedayzAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bedayzAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodoItemController(TodoContext context)
+        {
+            _context = context;
+        }
+        //get için
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+             var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+         }
+        //put için
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        [HttpPost]
+        public async Task<ActionResult <TodoItem >> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
+        [HttpDelete("{id")]
+        public async Task<ActionResult<TodoItem>> DeletleTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+            return todoItem;
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e=>e.Id==id);
+        }
+    }
+}
---
+++ Model/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using bedayzAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bedayzAPI.Model
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        //veritabanı ile ilişki kuracak hale getiriyor
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
